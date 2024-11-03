a021337a95025a3c384d21261b98e796ee888327
Umoh Ndifreke Macauley <hyylittle@gmail.com>
2024-11-03 16:45:39 +0100

    commit

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    username = models.CharField(max_length=255, blank=True, null=True, unique=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+
+
+
